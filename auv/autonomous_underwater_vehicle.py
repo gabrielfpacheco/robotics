e06733fd16c33932fe91b8c7f68b4430d8e0f8a5
gabrielfpacheco <gabriel.fpacheco@gmail.com>
2019-05-08 18:30:31 -0300

    Improving graphics

--- auv/autonomous_underwater_vehicle.py
+++ auv/autonomous_underwater_vehicle.py
@@ -125,8 +125,8 @@ class AutonomousUnderwaterVehicle:
                 plt.pause(interval)
 
             plt.show(block=True)
-            ani = animation.ArtistAnimation(fig, images, interval=interval, blit=True, repeat_delay=1000)
-            ani.save('.results/auv/3d_position.html', writer='html')
+            # ani = animation.ArtistAnimation(fig, images, interval=interval, blit=True, repeat_delay=1000)
+            # ani.save('.results/auv/3d_position.html', writer='html')
 
     @staticmethod
     def plot_position_profiles(trajectory, time, show_animation=True):
